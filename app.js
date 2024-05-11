3afe815563e34f45bc84eaefa6455c5fcb7e1bdb
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2024-05-11 12:21:19 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cors = require('cors');
+const appOne = express();
+
+// Middleware
+appOne.use(bodyParser.json());
+appOne.use(compression());
+appOne.use(cors());
+
+// Routes
+
+
+appOne.get('/',(req,res)=>{
+    console.log(process.env.DATABASE_URL)
+    res.send("Hello: DONE")
+  })  
+
+module.exports = appOne;
+
+// 
---
+++ server.js
@@ -0,0 +1,37 @@
+// const { PrismaClient } = require('@prisma/client');
+// // const express = require('express');
+// const prisma = new PrismaClient();
+
+
+// const app = express();
+const appOne = require('./app')
+
+
+
+// ...Other middleware and route configurations...
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+appOne.listen(PORT, async () => {
+  try {
+    // await prisma.$connect();
+
+    console.log(`Server running on port ${PORT}`);
+
+  } catch (error) {
+    console.error('Error connecting to database:', error);
+    process.exit(1);
+  }
+});
+
+// app.get('/',(req,res)=>{
+//   console.log("Test: it works")
+//   res.send("Hello: DONE")
+// })  
+
+
+// Graceful shutdown on process termination
+process.on('SIGINT', async () => {
+//   await prisma.$disconnect();
+  process.exit(0);
+});
