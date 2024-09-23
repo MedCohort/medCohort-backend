b2747c1e711da98f7b66b76b29d234780ece1c0b
Binamin-hussein100 <binamin.h.hassan14@gmail.com>
2024-09-23 17:59:18 +0300

    feat/ auth integration fixes

--- app.js
+++ app.js
@@ -17,12 +17,21 @@ appOne.use(bodyParser.json());
 appOne.use(compression());
 
 appOne.use(passportConfig.initialize());
+
+const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173'];
+
  
 appOne.use(cors({
-  origin: 'http://localhost:5173', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true,
+  
 }));
 
 // Routes
